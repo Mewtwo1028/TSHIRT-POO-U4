15e308c30e13fba5ca792e8191362938ea12b955
JUAN JESUS MATA SOLIS <jujemataso@ittepic.edu.mx>
2020-11-16 14:09:25 -0700

    Cambios extras

--- src/clases/App.java
+++ src/clases/App.java
@@ -17,4 +17,12 @@ public class App {
         return "APP{" + "url=" + url + '}';
     }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
 }
